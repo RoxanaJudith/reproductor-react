352107e083f0f7573d05b6336a9322ecc557f24c
RoxanaJudith <roxanajpr.4@gmail.com>
2018-02-12 21:57:08 -0400

    Ply/Pausa:Creando la UI

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,13 +2,26 @@ import React, {Component} from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component{
+	state={
+		pause:true,
+	}
+	togglePlay=(event)=>{
+		this.setState({
+			pause:!this.state.pause
+		})
+	}
 	render(){
 		return(
 			<VideoPlayerLayout>
 			<Title
 				title="Esto es un buen video"
+			/>
+			<PlayPause
+				pause={this.state.pause}
+				handleClick={this.togglePlay}
 			/>
 				<video
 				autoPlay={true}
